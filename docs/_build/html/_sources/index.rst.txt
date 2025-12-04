7361042c51f59614007f654f5bd199156b85cd6a
Amal K E <amalarun9@gmail.com>
2025-12-04 23:28:48 +0100

    Add Sphinx documentation and docs CI workflow

---
+++ docs/_build/html/_sources/index.rst.txt
@@ -0,0 +1,16 @@
+Welcome to mypkg's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+API Reference
+=============
+
+.. automodule:: newf
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
--- src/newf/__init__.py
+++ src/newf/__init__.py
@@ -1,3 +1,38 @@
-def add(a, b):
-    x = 1
+"""Utility functions for simple math operations."""
+
+def add(a: int, b: int) -> int:
+    """
+    Return the sum of two integers.
+
+    Parameters
+    ----------
+    a : int
+        First addend.
+    b : int
+        Second addend.
+
+    Returns
+    -------
+    int
+        The sum of ``a`` and ``b``.
+    """
     return a + b
+
+
+def multiply(a: int, b: int) -> int:
+    """
+    Return the product of two integers.
+
+    Parameters
+    ----------
+    a : int
+        First factor.
+    b : int
+        Second factor.
+
+    Returns
+    -------
+    int
+        The product of ``a`` and ``b``.
+    """
+    return a * b
